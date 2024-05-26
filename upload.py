0273eea79862a92ec6c0d4b3d6352d7f385e4f7d
Zander Lewis <wolfthedev@gmail.com>
2024-05-26 09:24:31 -0400

    Update upload.py
    Add safety message.

--- upload.py
+++ upload.py
@@ -1,12 +1,19 @@
 import subprocess
 import os
+import sys
 
-if os.path.exists("dist"):
-    subprocess.run(["rm", "-r", "dist"])
-if os.path.exists("build"):
-    subprocess.run(["rm", "-r", "build"])
-if os.path.exists("llamascript.egg-info"):
-    subprocess.run(["rm", "-r", "llamascript.egg-info"])
+i = input("Are you sure you don't want to create a gh release instead? (Y/n): ")
 
-subprocess.run(["python3", "setup.py", "sdist", "bdist_wheel"])
-subprocess.run(["twine", "upload", "dist/*"])
+if i == "n":
+    sys.exit(0)
+elif i == "Y":
+    if os.path.exists("dist"):
+        subprocess.run(["rm", "-r", "dist"])
+    if os.path.exists("build"):
+        subprocess.run(["rm", "-r", "build"])
+    if os.path.exists("llamascript.egg-info"):
+        subprocess.run(["rm", "-r", "llamascript.egg-info"])
+    
+    subprocess.run(["python3", "setup.py", "sdist", "bdist_wheel"])
+    subprocess.run(["twine", "upload", "dist/*"])
+sys.exit(0)
